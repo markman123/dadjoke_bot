e29b096039354bb5d3a75dd0c199cf5b9fd411c5
Mark Marlow <theMarlzy@gmail.com>
2022-02-04 12:56:45 +1100

    changed to reply and support edits

--- start_bot.py
+++ start_bot.py
@@ -5,19 +5,28 @@ import os
 import discord
 import requests
 
-
+COMMAND = "!joke"
 dotenv.load_dotenv()
 
-async def send_a_joke(client):
+async def maybe_send_a_joke(client, message, target_channel):
+    if COMMAND in message.content.lower() and message.channel.id == int(target_channel):
+        logging.debug(f"Detected {COMMAND}")
+        await send_a_joke(message)
+
+
+async def send_a_joke(message):
     joke = requests.get("https://icanhazdadjoke.com/",headers={"Accept": "text/plain"})
     if not joke.ok:
         logging.error(f"Failed getting joke.. response: {joke.status_code}:{joke.text}")
         return
-    channel_id = int(os.environ['CHANNEL_ID'])
-    channel = client.get_channel(channel_id)
-    await channel.send(joke.text)
+    await message.reply(joke.text)
+    logging.debug(f"Sent joke: {joke.text}")
 
 def start_bot():
+    target_channel = os.environ['PROD_CHANNEL_ID']
+    if os.environ.get('BOT_TEST'):
+        logging.info("Detected testing environment")
+        target_channel = os.environ['TEST_CHANNEL_ID']
     client = discord.Client()
 
     @client.event
@@ -26,8 +35,11 @@ def start_bot():
 
     @client.event
     async def on_message(message):
-        if "joke" in message.content.lower() and message.channel.id == int(os.environ['CHANNEL_ID']):
-            await send_a_joke(client)
+        await maybe_send_a_joke(client, message, target_channel)
+
+    @client.event
+    async def on_message_edit(_before, after):
+        await maybe_send_a_joke(client, after, target_channel)
 
     token = os.environ['DISCORD_TOKEN']
     client.run(token)
